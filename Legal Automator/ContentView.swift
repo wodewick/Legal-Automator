920c0945579039f8d26cf1d55bbf3ba62532f6f6
wodewick <rod@serkowski.net>
2025-07-27 10:53:47 +1000

    Initial Commit

---
+++ Legal Automator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Legal Automator
+//
+//  Created by Rodney Serkowski on 27/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Legal Automator/Legal_AutomatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Legal_AutomatorApp.swift
+//  Legal Automator
+//
+//  Created by Rodney Serkowski on 27/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct Legal_AutomatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
